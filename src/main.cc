89c2de9df56f4bf6d61e75065f9085dbd4224a10
MemoCheLenovo <gmanriquev@unsa.edu.pe>
2026-07-07 13:31:46 -0500

    operadores

--- src/main.cc
+++ src/main.cc
@@ -4,25 +4,23 @@ using namespace std;
 
 int main() {
     int base[] = {10, 20, 30};
+
     ArrayDin ad(base, 3);
-    cout << ad.getSize() << endl;
 
-    ad.push_back(40);
-    cout << ad.getSize() << endl;
-
-    ad.insert(1, 15);
-    cout << ad.getSize() << endl;
-
-    ad.remove(0);
-    cout << ad.getSize() << endl;
     int base2 [] = {40,50, 60};
 
     ArrayDin ad2 (base2,3);
 
     ArrayDin ad3 = ad + ad2; // operator +
-                             //
-    cout << (ad == ad2);
+                             
+    cout << ad3 << endl; // operator <<
+                         //
+    cout << ad3.getSize() << endl;
 
-    cout << ad3 ; // operator <<
+    cout << (ad == ad2) << endl;
+
+    cout << ad ;
+
+    cout << ad2;
     return 0;
 }
